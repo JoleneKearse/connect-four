3e3d56e9821da36c0cc483b7433435bd8712da40
JoleneKearse <pharaohnutz@gmail.com>
2025-03-20 20:13:50 -0400

    feat: start on rules page

--- src/pages/RulesPage.tsx
+++ src/pages/RulesPage.tsx
@@ -1,7 +1,22 @@
 const RulesPage = () => {
   return (
-    <main>
-      <h1>Rules Page</h1>
+    <main className="rules-container">
+      <h1 className="rules-heading">Rules</h1>
+      <h2 className="rules-subheading">Objective</h2>
+      <p className="rules-text" style={{ textTransform: "none" }}>
+        Be the first player to connect 4 of the same colored discs in a row
+        (either vertically, horizontally, or diagonally).
+      </p>
+      <h2 className="rules-subheading">How To Play</h2>
+      <ol className="rules-text rules-list" style={{ textTransform: "none" }}>
+        <li>Red goes first in the first game.</li>
+        <li>
+          Players must alternate turns, and only one disc can be dropped in each
+          turn.
+        </li>
+        <li>The game ends when there is a 4-in-a-row or a stalemate.</li>
+        <li>The starter of the previous game goes second on the next game.</li>
+      </ol>
     </main>
   );
 };
