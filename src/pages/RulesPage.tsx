a91198b460218504d3aff9b90899b9620ff0954e
JoleneKearse <pharaohnutz@gmail.com>
2025-03-23 17:11:45 -0400

    chore: pixel perfected rulespage

--- src/pages/RulesPage.tsx
+++ src/pages/RulesPage.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import check from "/images/icon-check.svg";
+
 const RulesPage = () => {
   return (
     <main className="rules-container">
@@ -17,6 +20,9 @@ const RulesPage = () => {
         <li>The game ends when there is a 4-in-a-row or a stalemate.</li>
         <li>The starter of the previous game goes second on the next game.</li>
       </ol>
+      <Link to="/">
+        <img src={check} alt="back to home" className="rules-link" />
+      </Link>
     </main>
   );
 };
